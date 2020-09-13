0da43ad6f7bc1c8a6b61184a3dffc77a31cfa2fb
gamesguru <mathmuncher11@gmail.com>
2020-09-13 12:30:48 -0400

    rename main activity, git add

--- app/src/main/java/co/nutra/nt/OnBoardActivity.kt
+++ app/src/main/java/co/nutra/nt/OnBoardActivity.kt
@@ -3,7 +3,7 @@ package co.nutra.nt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OnBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
