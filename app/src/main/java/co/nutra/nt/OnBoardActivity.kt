7aa12bc355c517fd531f99ffe91aca118f69ad59
gamesguru <mathmuncher11@gmail.com>
2020-09-14 04:27:36 -0400

    more

--- app/src/main/java/co/nutra/nt/OnBoardActivity.kt
+++ app/src/main/java/co/nutra/nt/OnBoardActivity.kt
@@ -3,12 +3,14 @@ package co.nutra.nt
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.StrictMode
 import android.view.View
-import android.widget.ProgressBar
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
@@ -19,12 +21,34 @@ class OnBoardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
+//        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+//        StrictMode.setThreadPolicy(policy)
     }
 
     fun quit(view: View) {
         finishAndRemoveTask()
+    }
+
+    private fun log(line: String) {
+        val textView = TextView(this)
+        textView.text = line
+        textView.layoutParams =
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+//        runOnUiThread {
+        // Add line
+        linearLayoutConsole.addView(textView)
+
+        // Scroll
+        scrollViewConsole.post {
+            scrollViewConsole.fullScroll(
+                ScrollView.FOCUS_DOWN
+            )
+        }
+//        }
     }
 
     fun retry(view: View) {
@@ -35,14 +59,17 @@ class OnBoardActivity : AppCompatActivity() {
         // Download, unpack database
         val downloadsOverviewApiUrl =
             "https://api.bitbucket.org/2.0/repositories/dasheenster/nutra-utils/downloads"
-//        val downloadUrl = "${downloadsOverviewApiUrl}/${targetFileName}"
 
-        val progressBar: ProgressBar = findViewById(R.id.progressBar)
-        // Make overview network call
-        progressBar.visibility = ProgressBar.VISIBLE;
-        val downloadsOverviewStr = URL(downloadsOverviewApiUrl).readText();
-        progressBar.visibility = ProgressBar.INVISIBLE
+        // Make "overview" network call
+        var downloadsOverviewStr = String()
+        val ovThread = Thread {
+            downloadsOverviewStr = URL(downloadsOverviewApiUrl).readText();
+        }
+        log("GET $downloadsOverviewApiUrl")
+        ovThread.start()
+        ovThread.join()
 
+        // Find the target file, if it exists
         val downloadsOverview: JSONArray = JSONObject(downloadsOverviewStr)["values"] as JSONArray
         val targetFileName = "nutra.db-${Settings.dbTarget}.tar.xz"
         var targetFile = JSONObject()
@@ -59,7 +86,7 @@ class OnBoardActivity : AppCompatActivity() {
         if (targetFile.length() == 0)
             Toast.makeText(
                 applicationContext,
-                "ERROR: download doesn't exist, contact support: ${targetFileName}",
+                "ERROR: download doesn't exist, contact support: $targetFileName",
                 Toast.LENGTH_LONG
             ).show()
         else {
@@ -67,25 +94,35 @@ class OnBoardActivity : AppCompatActivity() {
             val creationDateStr = targetFile["created_on"].toString()
             val size = targetFile["size"] as Int
             val sizeStr = "%.3f".format(size / 1000000.0)
-            showAlertDialogButtonClicked(view, nDownloads, creationDateStr, sizeStr)
+            val downloadUrl = "$downloadsOverviewApiUrl/$targetFileName"
+
+            // Prompt user to download large db file (> 5MB)
+            showAlertDialogButtonClicked(view, downloadUrl, nDownloads, creationDateStr, sizeStr)
         }
     }
 
-    fun showAlertDialogButtonClicked(
+    private fun showAlertDialogButtonClicked(
         view: View,
+        downloadUrl: String,
         nDownloads: Int,
         creationDate: String,
         size: String
-    ) {        // setup the alert builder
+    ) {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle("Download large file?")
         val message =
             "Download nutra.db v${Settings.dbTarget}?\n\nSize: ${size}MB\nDownloads: ${nDownloads}\n\nCreated: ${creationDate}"
         builder.setMessage(message)
-        builder.setPositiveButton("OK", null)
+        builder.setPositiveButton("OK") { _, _ ->
+            startDownload(downloadUrl)
+        }
         builder.setNegativeButton("Cancel", null)
 
         val dialog: AlertDialog = builder.create()
         dialog.show()
+    }
+
+    private fun startDownload(url: String) {
+
     }
 }
